a5d899d9ed715cc3bf9787f4fcf0aee61c24e6fa
Natalia Azevedo <natalia.azevedo@printi.com.br>
2018-04-26 19:04:33 -0300

    Doing the clock component

--- app/src/components/Clock.jsx
+++ app/src/components/Clock.jsx
@@ -1,13 +1,26 @@
 import React from 'react';
 import styled from 'styled-components';
+import moment from 'moment';
+// https://gist.github.com/igmoweb/f1b26c5a8870d8f30bbb0cc348ee617b cronômetro
+//https://www.codigofonte.com.br/codigos/contagem-regressiva
 
-
+// format using momment https://stackoverflow.com/questions/16129157/countdown-timer-using-moment-js?utm_medium=organic&utm_source=google_rich_qa&utm_campaign=google_rich_qa
 class Clock extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0,
+    };
+  }
 
   render() {
+    console.log(setTimeout(function() {
+      console.log("setTimeout: Ja passou 3 minutos!");
+    }, 180000));
+
     return (
       <div>
-        clock
+        {moment().format('LTS')}
       </div>
     );
   }

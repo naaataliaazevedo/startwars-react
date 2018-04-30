810200456dfef22cc43c64130fb678008e9c945a
Natalia Azevedo <natalia.azevedo@printi.com.br>
2018-04-30 17:38:24 -0300

    Removed the comment line

--- app/src/App.js
+++ app/src/App.js
@@ -16,13 +16,6 @@ class App extends Component {
           <Route path="/characters" component={Characters} />
         </div>
       </Router>
-      {/*<header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-    </p>*/}
       </div>
     );
   }
